3cf0175aa0b659739bc4eb98644b4675fc0be86a
Simge Keser <s84167444@china.huawei.com>
2020-09-07 10:00:04 +0300

    updated

--- app/src/main/java/com/simge/safetydetectproject/MainActivity.java
+++ app/src/main/java/com/simge/safetydetectproject/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import com.huawei.hms.support.api.safetydetect.SafetyDetect;
 import com.huawei.hms.support.api.safetydetect.SafetyDetectClient;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private final String APP_ID="102841133";
     private Button btnSysIntegrity,btnAppsCheck,btnUserDetect,btnURLCheck,btnWifiCheck;
@@ -32,41 +32,38 @@ public class MainActivity extends AppCompatActivity{
         btnURLCheck=findViewById(R.id.buttonURLCheck);
         btnWifiCheck=findViewById(R.id.buttonWifiCheck);
 
-         btnSysIntegrity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-               sysIntegrity= new SysIntegrity(client,MainActivity.this,APP_ID);
-               sysIntegrity.invoke();
-            }
-        });
-
-         btnAppsCheck.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-               appsCheck=new AppsCheck(client,MainActivity.this,APP_ID);
-               appsCheck.invokeGetMaliciousApps();
-             }
-         });
-         btnUserDetect.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
+        btnSysIntegrity.setOnClickListener(this);
+        btnAppsCheck.setOnClickListener(this);
+        btnUserDetect.setOnClickListener(this);
+        btnURLCheck.setOnClickListener(this);
+        btnWifiCheck.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSysIntegrity:
+                sysIntegrity= new SysIntegrity(client,MainActivity.this,APP_ID);
+                sysIntegrity.invoke();
+                break;
+            case R.id.buttonAppsCheck:
+                appsCheck=new AppsCheck(client,MainActivity.this,APP_ID);
+                appsCheck.invokeGetMaliciousApps();
+                break;
+            case R.id.buttonURLCheck:
+                urlCheck=new URLCheck(client,MainActivity.this,APP_ID);
+                urlCheck.callUrlCheckApi();
+                break;
+            case R.id.buttonUserDetect:
                 userDetect=new UserDetect(MainActivity.this,APP_ID);
                 userDetect.detect();
-             }
-         });
-         btnURLCheck.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View view) {
-                 urlCheck=new URLCheck(client,MainActivity.this,APP_ID);
-                 urlCheck.callUrlCheckApi();
-             }
-         });
-        btnWifiCheck.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+                break;
+            case R.id.buttonWifiCheck:
                 wifiCheck=new WifiCheck(client,MainActivity.this,APP_ID);
                 wifiCheck.invokeGetWifiDetectStatus();
-            }
-        });
+                break;
+            default:
+                break;
+        }
     }
+
 }
